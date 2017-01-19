7bedd857d552114f19ca2246b45641f2e9d4f6be
RoyXue812 <RoyXue812@gmail.com>
2017-01-19 03:10:40 -0500

    initial XINU

---
+++ sys/main.c
@@ -0,0 +1,18 @@
+/* user.c - main */
+
+#include <conf.h>
+#include <kernel.h>
+#include <proc.h>
+#include <stdio.h>
+
+void halt();
+
+/*------------------------------------------------------------------------
+ *  main  --  user main program
+ *------------------------------------------------------------------------
+ */
+int main()
+{
+	kprintf("\n\nHello World, Xinu lives\n\n");
+	return 0;
+}
