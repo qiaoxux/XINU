4d209e68fec06df395a7891a0e27a7a6fa74c9d6
RoyXue812 <RoyXue812@gmail.com>
2017-04-18 19:43:44 -0400

    x

--- sys/main.c
+++ sys/main.c
@@ -30,15 +30,15 @@ void proc1_test1(char *msg, int lck) {
 	}
 
 	addr = (char*) PROC1_VADDR;
-	for (i = 0; i < 25; i++) {
+	for (i = 0; i < 26; i++) {
 		*(addr + i * NBPG) = 'A' + i;
 	}
 
-	sleep(6);
-
-	for (i = 0; i < 25; i++) {
+	for (i = 0; i < 26; i++) {
 		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + i * NBPG));
 	}
+
+	sleep(6);
 
 	xmunmap(PROC1_VPNO);
 	return;
@@ -95,5 +95,7 @@ int main() {
 	// resume(pid1);
 	// sleep(3);
 
+	shutdown();
+	
 	return 0;
 }
