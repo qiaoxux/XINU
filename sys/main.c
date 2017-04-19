b2991ffa3de3b91a051742f1cea43db9576b163b
RoyXue812 <RoyXue812@gmail.com>
2017-04-18 20:03:06 -0400

    x

--- sys/main.c
+++ sys/main.c
@@ -34,12 +34,12 @@ void proc1_test1(char *msg, int lck) {
 		*(addr + i * NBPG) = 'A' + i;
 	}
 
-	sleep(6);
+	
 
 	for (i = 0; i < 26; i++) {
 		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + i * NBPG));
 	}
-
+sleep(6);
 	xmunmap(PROC1_VPNO);
 	return;
 }
