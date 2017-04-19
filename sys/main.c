6e973ba7fa9dd01eb4b05f67496c092f9c3fd485
RoyXue812 <RoyXue812@gmail.com>
2017-04-19 12:06:53 -0400

    x

--- sys/main.c
+++ sys/main.c
@@ -79,16 +79,16 @@ int main() {
 	int pid1;
 	int pid2;
 
-	kprintf("\n1: shared memory\n");
-	pid1 = create(proc1_test1, 2000, 20, "proc1_test1", 0, NULL);
-	resume(pid1);
-	sleep(10);
-
-	// kprintf("\n2: vgetmem/vfreemem\n");
-	// pid1 = vcreate(proc1_test2, 2000, 100, 20, "proc1_test2", 0, NULL);
-	// kprintf("pid %d has private heap\n", pid1);
+	// kprintf("\n1: shared memory\n");
+	// pid1 = create(proc1_test1, 2000, 20, "proc1_test1", 0, NULL);
 	// resume(pid1);
-	// sleep(3);
+	// sleep(10);
+
+	kprintf("\n2: vgetmem/vfreemem\n");
+	pid1 = vcreate(proc1_test2, 2000, 100, 20, "proc1_test2", 0, NULL);
+	kprintf("pid %d has private heap\n", pid1);
+	resume(pid1);
+	sleep(3);
 
 	// kprintf("\n3: Frame test\n");
 	// pid1 = create(proc1_test3, 2000, 20, "proc1_test3", 0, NULL);
