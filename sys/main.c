efea38cebc1526555f49c9733c0b397f5a9c8907
RoyXue812 <RoyXue812@gmail.com>
2017-04-20 18:46:07 -0400

    x

--- sys/main.c
+++ sys/main.c
@@ -114,7 +114,10 @@ void test2() {
     pids[i] = create(proc_test2, 2000, 20, "proc_test2", 4, 1,
                      i*20, &ret, 0);
     resume(pids[i]);
-    
+    for (i = 0; i < NSTORES; i++) {
+      bsmap = &proctab[49].bsmap[i];
+      kprintf("bsm_lookup: %d %d %d %d %d \n", 49, i, bsmap->bs_status, bsmap->bs_vpno, bsmap->bs_npages);
+    }
     
   }
   sleep(3);
