41a737eb43ea76dbcf9e0075c06a508e2d0eca32
RoyXue812 <RoyXue812@gmail.com>
2017-04-20 18:56:55 -0400

    x

--- paging/paging.c
+++ paging/paging.c
@@ -68,8 +68,21 @@ SYSCALL init_page_directory_for_process(int pid) {
 	}
 	
 	proctab[pid].pdbr = (unsigned long) new_pd;
-	
+	if (pid == 47) {
+		bs_map_t *bsmap;
+		for (i = 0; i < NSTORES; i++) {
+			bsmap = &proctab[49].bsmap[i];
+			kprintf("bsm_lookup: %d %d %d %d %d \n", 49, i, bsmap->bs_status, bsmap->bs_vpno, bsmap->bs_npages);
+		}
+	}
 	init_bsmap_for_process(pid);
+	if (pid == 47) {
+		bs_map_t *bsmap;
+		for (i = 0; i < NSTORES; i++) {
+			bsmap = &proctab[49].bsmap[i];
+			kprintf("bsm_lookup: %d %d %d %d %d \n", 49, i, bsmap->bs_status, bsmap->bs_vpno, bsmap->bs_npages);
+		}
+	}
 	
 	restore(ps);
 	return OK;
