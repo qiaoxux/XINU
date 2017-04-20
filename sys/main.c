1fd314a469026f365869f89fd9071e85cba8bcf7
RoyXue812 <RoyXue812@gmail.com>
2017-04-20 19:28:13 -0400

    x

--- sys/main.c
+++ sys/main.c
@@ -99,6 +99,7 @@ void test2() {
   bsize = get_bs(1, 0);
   if (bsize != SYSERR)
     ret = TFAILED;
+  kprintf("0\n");
   mypid = create(proc_test2, 2000, 20, "proc_test2", 4, 1,
                  50, &ret, 4);
 
