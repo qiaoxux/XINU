9b714d79add314909acdff7bc9f2f8a1bcc7a61d
RoyXue812 <RoyXue812@gmail.com>
2017-04-20 22:29:15 -0400

    x

--- paging/pfint.c
+++ paging/pfint.c
@@ -28,6 +28,7 @@ SYSCALL pfint() {
     vp = p2vno(cr2);
     pd = proctab[currpid].pdbr;
 
+    kprintf("currpid vp: %d %d\n", currpid, vp);
     if( SYSERR == bsm_lookup(currpid, vp, &store, &pageth)) {
       	kprintf("pfint: virtual address hasn't been mapped!\n");
       	kill(currpid);
