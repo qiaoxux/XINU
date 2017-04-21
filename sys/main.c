024b274684a005a6a472b97e4050a2b2a457899e
RoyXue812 <RoyXue812@gmail.com>
2017-04-20 22:51:26 -0400

    x

--- sys/main.c
+++ sys/main.c
@@ -602,13 +602,13 @@ void testSC(){
 }
 
 int main() {
-	test1();
-	test2();
-	test3();
+	// test1();
+	// test2();
+	// test3();
 	// test4();
 	// test5();
 	// test6();
-	// test7();
+	test7();
 	// test8();
 	// test9();
 	// testSC();
